7146aaf40f88a70a875111fc7d01bc5b602f1316
liviaTI <liviasantferreira@gmail.com>
2025-06-15 22:54:07 -0300

    Deploy fullstrack React + Node no Heroku

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,36 +1,22 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const routes = require('./routes');
-
 
 const app = express();
 
-app.use(cors(
-    // origin: 'https://www.sit.com.br';
-));
+app.use(cors());
 app.use(express.json());
-/*
-    Rota/ Recurso
-
-    Métodos HTTPS
-    GET: Buscar/Listar uma informação do back-end
-    PUSH: Criar uma informação no back-end
-    PUT: Alterar informação no back-end
-    DELETE: Deletar uma informação do back-end
-
-    Tipos de paramêtros:
-    Query: Parâmetros nomeados enviados na roa após o "?" (filtos,paginação)
-    Route Params: Prâmetros utilizados para identificar recursos
-    Request Body: Corpo da requisição, utilizado para criar ou alterar recursos
-
-    Banco de Dados
-    SQL: MySQL, SQLite, PstgreSQL, Oracle, Microsoft SQL Server
-    noSQL: MongoDB, CouchDB
-
-    Como comunicar o back-end com o banco de daodos
-    Driver: SELECT * FROM users
-    Query Builder: table('users').select('*').where()
-    */
-
 app.use(routes);
-app.listen(3333); //3333 melhor porta para JS
+
+// Serve React buildado
+app.use(express.static(path.join(__dirname, '..', '..', 'frontend', 'build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', '..', 'frontend', 'build', 'index.html'));
+});
+
+const PORT = process.env.PORT || 3333;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
