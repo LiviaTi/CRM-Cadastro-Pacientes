c68f1f36c8ff75d6159a7f3cf8d3812ff73efb3a
liviaTI <liviasantferreira@gmail.com>
2025-06-17 00:41:46 -0300

    th

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,18 +5,33 @@ const routes = require('./routes');
 
 const app = express();
 
-app.use(cors());
+const allowedOrigins = [
+  'https://crm-production-app-aa858b59d6e5.herokuapp.com',
+];
+
+app.use(cors({
+  origin: function(origin, callback) {
+    if (!origin) return callback(null, true);  
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = `A política CORS bloqueou o acesso da origem ${origin}.`;
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  optionsSuccessStatus: 200,
+}));
+
 app.options('*', cors());
+
 app.use(express.json());
 app.use(routes);
 
-// Serve React buildado
+
 app.use(express.static(path.join(__dirname, '..', '..', 'frontend', 'build')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '..', '..', 'frontend', 'build', 'index.html'));
 });
-
 
 const PORT = process.env.PORT || 3333;
 app.listen(PORT, () => {
