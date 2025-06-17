81e8b0339c30460feee480071f0c81410204ce41
liviaTI <liviasantferreira@gmail.com>
2025-06-16 22:23:19 -0300

    Ajustando erros

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -34,7 +34,7 @@ module.exports = {
   production: {
     client: 'pg',
     connection: {
-      connectionString: process.env.DATABASE_URL,
+      connectionString: 'postgres://urofn7e7juhjd:p2d638776e4f8bb04831a4ca7ae27b563737cdfbb1122306002940ee2bdc06445@casrkuuedp6an1.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d82kiihhq01ve1',
       ssl: { rejectUnauthorized: false }
     },
     migrations: {
