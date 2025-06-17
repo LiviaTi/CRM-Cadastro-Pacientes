88fe536b4f310d31d074cacfc142a8de0d8a820c
liviaTI <liviasantferreira@gmail.com>
2025-06-17 00:32:16 -0300

    Testando

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,24 +5,7 @@ const routes = require('./routes');
 
 const app = express();
 
-const cors = require('cors');
-
-const allowedOrigins = [
-  'https://crm-production-app-aa858b59d6e5.herokuapp.com',
-];
-
-app.use(cors({
-  origin: function(origin, callback){
-    if(!origin) return callback(null, true); 
-    if(allowedOrigins.indexOf(origin) === -1){
-      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
-  credentials: true, 
-}));
-
+app.use(cors());
 app.options('*', cors());
 app.use(express.json());
 app.use(routes);
