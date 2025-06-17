0ff4bd5df1beb132625da6214f9ac230420c2866
liviaTI <liviasantferreira@gmail.com>
2025-06-17 01:15:46 -0300

    Ajustes

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,31 +9,38 @@ const allowedOrigins = [
   'https://crm-production-app-aa858b59d6e5.herokuapp.com',
 ];
 
+
 app.use((req, res, next) => {
-  console.log('Origin recebida:', req.headers.origin);
+  console.log('Origin:', req.headers.origin);
   next();
 });
 
-app.use(cors({
-  origin: function(origin, callback) {
-    if (!origin) return callback(null, true); 
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = `A política CORS bloqueou o acesso da origem ${origin}.`;
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
-  optionsSuccessStatus: 200,
-}));
 
-app.options('*', cors());
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  if (!origin) {
+    
+    res.header('Access-Control-Allow-Origin', '*');
+    return next();
+  }
+  if (allowedOrigins.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if (req.method === 'OPTIONS') {
+      return res.sendStatus(200);
+    }
+    return next();
+  } else {
+    return res.status(403).send('CORS policy: This origin is not allowed.');
+  }
+});
 
 app.use(express.json());
 app.use(routes);
 
 
 app.use(express.static(path.join(__dirname, '..', '..', 'frontend', 'build')));
-
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '..', '..', 'frontend', 'build', 'index.html'));
 });
