8bc48c6e40ec8053e71ba21caa2cbda4748277cb
liviaTI <liviasantferreira@gmail.com>
2025-06-17 13:26:35 -0300

    refactor: Adota o pacote cors para gerenciar permissoes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,49 +1,40 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
 
 const allowedOrigins = [
   'https://crm-production-app-aa858b59d6e5.herokuapp.com',
+  'http://localhost:3000'
 ];
 
-app.use((req, res, next) => {
-  console.log('Origin:', req.headers.origin);
-  next();
-});
-
-app.use((req, res, next) => {
-  const origin = req.headers.origin;
-
-  if (!origin) {
-    res.header('Access-Control-Allow-Origin', '*');
-    return next();
-  }
-
-  if (allowedOrigins.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-
-    if (req.method === 'OPTIONS') {
-      return res.sendStatus(200);
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
     }
+  },
+  methods: 'GET,POST,PUT,PATCH,DELETE,OPTIONS',
+  allowedHeaders: 'Content-Type, Authorization'
+};
 
-    return next();
-  }
-
-  return res.status(403).send(`CORS policy: Origin ${origin} not allowed`);
-});
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(routes);
 
 
 app.use(express.static(path.join(__dirname, '..', '..', 'frontend', 'build')));
+
+
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '..', '..', 'frontend', 'build', 'index.html'));
 });
+
 
 const PORT = process.env.PORT || 3333;
 app.listen(PORT, () => {
